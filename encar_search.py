db09381391ab2e674662450710bd4c4ecf884a31
harry@DESKTOP-V6L <l.bangsg@gmail.com>
2024-05-03 11:46:46 +0900

    Add encar_search.py

---
+++ encar_search.py
@@ -0,0 +1,66 @@
+import time
+import requests
+from config import get_delay_seconds
+
+
+def get_query(maker: str, model: str, submodel: str, **conditions: dict) -> str:
+    default = f"(And.Hidden.N._.(C.CarType.Y._.(C.Manufacturer.{maker}._.(C.ModelGroup.{model}._.Model.{submodel}.)))"
+    primary_conditions = ["year", "mileage", "price"]
+    for cond, cond_data in conditions.items():
+        if cond in primary_conditions:
+            start = cond_data.get("start", "")
+            end = cond_data.get("end", "")
+            default += f"_.{cond.title()}.range({start}..{end})."
+        elif cond == "options":
+            options = [f"_.{cond.title()}.{option}." for option in cond_data]
+            default += "".join(options)
+    closing = "_.SellType.일반._.Condition.Inspection._.Condition.Record.)"
+    return default + closing
+
+
+def get_all(header: dict, query: str, target_vehicle: list) -> dict:
+    start = 0
+    increase_by = 100
+    search_url = "http://api.encar.com/search/car/list/premium?count=true&q="
+    sort_range = f"&sr=|PriceDesc|{start}|{increase_by}"
+    url = search_url + query + sort_range
+
+    r = requests.get(url, headers=header)
+    if r.status_code != 200:
+        return r.raise_for_status()
+    remaining_cars = r.json().get("Count")
+    checked_cars_ids = {}
+
+    while remaining_cars > start:
+        sort_range = f"&sr=|PriceDesc|{start}|{increase_by}"
+        url = search_url + query + sort_range
+        time.sleep(get_delay_seconds())
+        r = requests.get(url, headers=header)
+        if r.status_code != 200:
+            return r.raise_for_status()
+        fetched_cars = r.json().get("SearchResults")
+        for car in fetched_cars:
+            car_id = car.get("Id", "")
+            car_data = {
+                car_id: {
+                    "Badge": car.get("Badge", ""),
+                    "BadgeDetail": car.get("BadgeDetail", ""),
+                    "Transmission": car.get("Transmission", ""),
+                    "FuelType": car.get("FuelType", ""),
+                    "Year": car.get("Year", ""),
+                    "FormYear": car.get("FormYear", ""),
+                    "Mileage": car.get("Mileage", ""),
+                    "Price": car.get("Price", ""),
+                    "OfficeCityState": car.get("OfficeCityState", ""),
+                    "ModifiedDate": car.get("ModifiedDate", ""),
+                    "Availability": 1,
+                    "InsuranceInspection": -1,
+                    "Maker": target_vehicle[0],
+                    "Model": target_vehicle[1],
+                    "Submodel": target_vehicle[2],
+                }
+            }
+            checked_cars_ids.update(car_data)
+        start += increase_by
+
+    return checked_cars_ids
